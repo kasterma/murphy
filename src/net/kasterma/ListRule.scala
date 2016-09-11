87c52157684424f7246b56a252b6598cac76dbb5
Bart Kastermans <kasterma@kasterma.net>
2016-09-11 15:51:41 +0200

    more flexible prob functions for ListRule

--- src/net/kasterma/ListRule.scala
+++ src/net/kasterma/ListRule.scala
@@ -9,6 +9,8 @@ class ListRule(member: Int, members: Int*) extends Rule {
 val _members: List[Int] = List(member) ++ members
 
   def size: Int = _members.length
-  def prob(x: Int): Double = if(_members contains x) 1.0 / size.toDouble else 0.0
+  def prob(x: Int): Double = if (_members contains x) 1.0 / size.toDouble else 0.0
+  def prob(xs: Int*): Double = prob(xs.toList)
+  def prob(xs: List[Int]): Double = xs map prob product
   override def toString: String = _members.mkString(" ")
 }
